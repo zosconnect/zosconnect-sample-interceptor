501c4c8c6ac2ad1e80d5fcbf8b44a3932eba89cf
Andrew Smithson <smithson@uk.ibm.com>
2015-08-27 21:14:11 +0100

    Add copyright and license to source files

--- src/com.ibm.crshnburn.zosconnect.interceptor/src/com/ibm/crshnburn/zosconnect/interceptor/SimpleInterceptorImpl.java
+++ src/com.ibm.crshnburn.zosconnect.interceptor/src/com/ibm/crshnburn/zosconnect/interceptor/SimpleInterceptorImpl.java
@@ -1,3 +1,19 @@
+/*
+ Copyright IBM Corporation 2015
+ 
+ LICENSE: Apache License
+          Version 2.0, January 2004
+          http://www.apache.org/licenses/
+          
+ The following code is sample code created by IBM Corporation.
+ This sample code is not part of any standard IBM product and
+ is provided to you solely for the purpose of assisting you in
+ the development of your applications.  The code is provided
+ 'as is', without warranty or condition of any kind.  IBM shall
+ not be liable for any damages arising out of your use of the
+ sample code, even if IBM has been advised of the possibility
+ of such damages.
+*/
 package com.ibm.crshnburn.zosconnect.interceptor;
 
 import java.security.Principal;
